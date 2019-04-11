d1ef8102ac98b25511c665be5f0fe4b0f2898c99
Rapid D <D.Rapid@gmail.com>
2019-04-11 13:01:34 +0300

    optimization

--- for.RnQ/RQThemes.pas
+++ for.RnQ/RQThemes.pas
@@ -456,11 +456,8 @@ implementation
   RnQDialogs,
   CommCtrl, mmSystem, Types;
 type
-//  Tsection=(_null,_roaster,_tip,_pics,_icons,_history,_smiles,_sounds,_menu);
   TRQsection=(_null, _pics, _icons, _smiles, _sounds, _ico, _smile, _str, _desc, _fontfile);
 const
-//  sectionLabels:array [Tsection] of string=('','roaster','tip','pics','icons',
-//    'history','smiles','sounds','menu');
   RQsectionLabels: array [TRQsection] of AnsiString=('', 'pics', 'icons', 'smiles',
     'sounds', 'rnqpics', 'rnqsmiles', 'strings', 'desc', 'font');
 
@@ -3304,66 +3301,74 @@ var
   var
     i: integer;
     s: AnsiString;
+    kp: AnsiString;
     fontProp: TFontObj;
   begin
-    result := true;
+    Result := true;
     fontProp := TFontObj.Create;
-    if k=prefix+'.name' then
+    if StartsStr(prefix, k) then
       begin
-       fontProp.flags := fontProp.flags or FPT_NAME;
-       s := fontAvailable(v);
- {$IFDEF UNICODE}
-//       fontProp.name := AnsiStrAlloc(Length(s)+1);
-       fontProp.name := WideStrAlloc(Length(s)+1);
- {$ELSE nonUNICODE}
-       fontProp.name := StrAlloc(Length(s)+1);
- {$ENDIF UNICODE}
-       StrPCopy(fontProp.name, String(s));
-  //     font.name := fontAvailable(v);
-      end
-    else
-    if k=prefix+'.size' then
-      begin
-       fontProp.flags := fontProp.flags or FPT_SIZE;
-       fontProp.size := strToIntA(v)
-  //     font.size := strToInt(v)
-      end
-    else
-    if k=prefix+'.color' then
-      begin
-       fontProp.flags := fontProp.flags or FPT_COLOR;
-       fontProp.color := str2color(v)
-  //     font.color := str2color(v)
-      end
-    else
-    if k=prefix+'.charset' then
-     begin
-       fontProp.flags := fontProp.flags or FPT_CHARSET;
-      if isOnlyDigits(v) then
-        fontProp.charset := strToIntA(v)
-  //      font.charset := strToInt(v)
-      else
-        if IdentToCharset(String(v), i) then
-          fontProp.charset := i
-  //        font.charset:=i
+        kp := copy(k, Length(prefix)+1, 2000);
+
+        if kp='.name' then
+          begin
+           fontProp.flags := fontProp.flags or FPT_NAME;
+           s := fontAvailable(v);
+     {$IFDEF UNICODE}
+    //       fontProp.name := AnsiStrAlloc(Length(s)+1);
+           fontProp.name := WideStrAlloc(Length(s)+1);
+     {$ELSE nonUNICODE}
+           fontProp.name := StrAlloc(Length(s)+1);
+     {$ENDIF UNICODE}
+           StrPCopy(fontProp.name, String(s));
+      //     font.name := fontAvailable(v);
+          end
         else
-          if IdentToCharset(String(v+'_CHARSET'), i) then
-            fontProp.charset := i
-  //          font.charset:=i
-           else
-            begin
-              Result := False;
-  //            Exit;
-            end;
-     end
-    else
-    if k=prefix+'.style' then
-      begin
-       fontProp.flags := fontProp.flags or FPT_STYLE;
-       fontProp.style := str2fontstyle(v)
-  //     font.style:=str2fontstyle(v)
+        if kp='.size' then
+          begin
+           fontProp.flags := fontProp.flags or FPT_SIZE;
+           fontProp.size := strToIntA(v)
+      //     font.size := strToInt(v)
+          end
+        else
+        if kp='.color' then
+          begin
+           fontProp.flags := fontProp.flags or FPT_COLOR;
+           fontProp.color := str2color(v)
+      //     font.color := str2color(v)
+          end
+        else
+        if kp='.charset' then
+         begin
+           fontProp.flags := fontProp.flags or FPT_CHARSET;
+          if isOnlyDigits(v) then
+            fontProp.charset := strToIntA(v)
+      //      font.charset := strToInt(v)
+          else
+            if IdentToCharset(String(v), i) then
+              fontProp.charset := i
+      //        font.charset:=i
+            else
+              if IdentToCharset(String(v+'_CHARSET'), i) then
+                fontProp.charset := i
+      //          font.charset:=i
+               else
+                begin
+                  Result := False;
+      //            Exit;
+                end;
+         end
+        else
+        if kp='.style' then
+          begin
+           fontProp.flags := fontProp.flags or FPT_STYLE;
+           fontProp.style := str2fontstyle(v)
+      //     font.style:=str2fontstyle(v)
+          end
+         else
+          result := false;
       end
-    else
+     else
       result := false;
     if Result then
       addprop(ppar, fontProp);
